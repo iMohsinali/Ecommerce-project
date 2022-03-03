29e458c6f04708c27c264eb741dcb391a71093aa
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-03-03 10:58:41 -0500

    Create Rating.js

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Rating = ({rating,rewives}) => {
+  return (
+    <div class="rating">
+    <span>
+        <i className={rating >=1?"fa fa-star": rating>=0.5?"fa fa-star-half-o":'fa fa-star-o'}/>
+       
+
+    </span>
+    <span>
+        <i className={rating >=2?"fa fa-star": rating>=1.5?"fa fa-star-half-o":'fa fa-star-o'}/>
+       
+
+    </span>
+    
+    <span>
+        <i className={rating >=3?"fa fa-star": rating>=2.5?"fa fa-star-half-o":'fa fa-star-o'}/>
+       
+
+    </span>
+    <span>
+        <i className={rating >=4?"fa fa-star": rating>=3.5?"fa fa-star-half-o":'fa fa-star-o'}/>
+       
+
+    </span>
+    <span>
+        <i className={rating >=5?"fa fa-star": rating>=4.5?"fa fa-star-half-o":'fa fa-star-o'}/>
+       
+
+    </span>
+      <span>
+          {rewives + 'rewives'}
+      </span>
+
+
+</div>
+  )
+}
+
+export default Rating
